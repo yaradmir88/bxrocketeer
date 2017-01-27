70c5c65a840b86228bc4b4d8a2a7c1669809e919
Виталий Акмашев <va@crtweb.ru>
2017-01-27 16:31:37 +0500

    add more info to bitrix check

--- src/CheckBitrixDeploy.php
+++ src/CheckBitrixDeploy.php
@@ -7,14 +7,14 @@ class CheckBitrixDeploy extends \Rocketeer\Abstracts\AbstractTask
     /**
      * @var string
      */
-    protected $description = 'Check whether we can or on continue deploy';
+    protected $description = 'Check whether we can or not continue deploy';
 
     public function execute()
     {
         $overload = ini_get('mbstring.func_overload');
         if ($overload !== '0') {
             $this->command->error(
-                'mbstring.func_overload is set to '.$overload.'. Change it to 0 in your php.ini.'
+                'mbstring.func_overload is set to '.$overload.'. Change it to 0 in your php.ini: '.php_ini_loaded_file()
             );
             $this->halt();
 
