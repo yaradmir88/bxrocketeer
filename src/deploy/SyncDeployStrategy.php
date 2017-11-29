dfe6d2252e480d626e2bf80f8ea8be315751029d
Виталий Акмашев <va@crtweb.ru>
2017-11-29 16:30:21 +0500

    fix rsync strategy for rocketeer 2.6

--- src/deploy/SyncDeployStrategy.php
+++ src/deploy/SyncDeployStrategy.php
@@ -2,6 +2,7 @@
 
 namespace bxrocketeer\deploy;
 
+use Rocketeer\Bash;
 use Rocketeer\Strategies\Deploy\SyncStrategy;
 
 /**
@@ -10,9 +11,33 @@ use Rocketeer\Strategies\Deploy\SyncStrategy;
 class SyncDeployStrategy extends SyncStrategy
 {
     /**
-     * @var array
+     * {@inheritdoc}
      */
-    protected $options = [
-        'excluded' => ['.git'],
-    ];
+    protected function rsyncTo($destination, $source = './')
+    {
+        // Build host handle
+        $arguments = [];
+        $handle = $this->getSyncHandle();
+
+        // Create options
+        $options = ['--verbose' => null, '--recursive' => null, '--rsh' => 'ssh', '--compress' => null];
+
+        // Create SSH command
+        $options['--rsh'] = $this->getTransport();
+
+        // Build arguments
+        $arguments[] = $source;
+        $arguments[] = $handle.':'.$destination;
+
+        // Set excluded files and folders
+        $options['--exclude'] = ['.git'];
+
+        // Create binary and command
+        $rsync = $this->binary('rsync');
+        $command = $rsync->getCommand(null, $arguments, $options);
+
+        return $this->bash->onLocal(function (Bash $bash) use ($command) {
+            return $bash->run($command);
+        });
+    }
 }
