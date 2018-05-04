a58b654c636b01aa17d605d76d8927e1a34fdeaf
marvin255 <postbox433@gmail.com>
2018-05-04 16:03:55 +0500

    fix comments in check bitrix deploy

--- src/CheckBitrixDeploy.php
+++ src/CheckBitrixDeploy.php
@@ -2,6 +2,9 @@
 
 namespace bxrocketeer;
 
+/**
+ * Проверяет можно ли запустить деплой на данной локальной машине.
+ */
 class CheckBitrixDeploy extends \Rocketeer\Abstracts\AbstractTask
 {
     /**
@@ -9,18 +12,24 @@ class CheckBitrixDeploy extends \Rocketeer\Abstracts\AbstractTask
      */
     protected $description = 'Check whether we can or not continue deploy';
 
+    /**
+     * @inheritdoc
+     */
     public function execute()
     {
+        $result = true;
         $overload = ini_get('mbstring.func_overload');
+
         if ($overload !== '0') {
             $this->command->error(
-                'mbstring.func_overload is set to ' . $overload . '. Change it to 0 in your php.ini: ' . php_ini_loaded_file() . ' or run rocketeer like so: php -f rocketeer.phar -d mbstring.func_overload=0 update'
+                'mbstring.func_overload is set to ' . $overload
+                . '. Change it to 0 in your php.ini: ' . php_ini_loaded_file()
+                . ' or run rocketeer like so: php -f rocketeer.phar -d mbstring.func_overload=0 update'
             );
             $this->halt();
-
-            return false;
+            $result = false;
         }
 
-        return true;
+        return $result;
     }
 }
